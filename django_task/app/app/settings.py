80369ff6c4db47c8a82d5210a1e0610c4b124846
Hari1642 <harivalle79@gmail.com>
2024-11-19 22:25:33 +0530

    django_task/app/app/settings.py

--- django_task/app/app/settings.py
+++ django_task/app/app/settings.py
@@ -129,3 +129,21 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
